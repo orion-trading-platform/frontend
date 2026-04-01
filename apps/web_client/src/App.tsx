e49908f0ab99d98c3be56e5d1fed000d4738aebf
RogerDai2026 <qd8@rice.edu>
2026-04-01 14:19:49 -0500

    erge with main to get auth and write bypass auth to check ordering

--- apps/web_client/src/App.tsx
+++ apps/web_client/src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Login from './features/auth/Login';
 import ResetPassword from './features/auth/ResetPassword';
 import { AuthProvider } from './features/auth';
-// import Dashboard from './features/dashboard/DashboardPage';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { OrderingPage } from './features/ordering/OrderingPage';
 
 const App: React.FC = () => {
   return (
@@ -11,10 +11,12 @@ const App: React.FC = () => {
       <AuthProvider>
         <div className="app-container">
           <Routes>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/reset-password" element={<ResetPassword />} />
-            {/* <Route path="/dashboard" element={<DashboardPage />} />
-            <Route path="*" element={<DashboardPage />} /> */}
+            <Route path="/dashboard" element={<OrderingPage />} />
+            <Route path="/trade" element={<OrderingPage />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Routes>
         </div>
       </AuthProvider>
